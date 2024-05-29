89f423241059ba1c24856279ee61c654c918526d
biltormenta <161093201+biltormenta@users.noreply.github.com>
2024-05-29 18:18:22 -0500

    creados ajustes.jsx
    importados de html a jsx

---
+++ src/pages/ajustes.jsx
@@ -0,0 +1,134 @@
+import React from 'react';
+import '../assets/Ajustes.css';
+
+function Ajustes() {
+  return (
+    <div className="ajustes root">
+      <img className="decorator" src="../assets/fa01703fb6148402c3eb6c77c4d61744.svg" alt="alt text" />
+      <img className="decorator1" src="../assets/e1b8ada29c3ceeff562d2741c9b81e17.svg" alt="alt text" />
+      <div className="group">
+        <div className="rect"></div>
+        <div className="wrapper">
+          <div className="wrapper1">
+            <div className="wrapper11">
+              <h2 className="wrapper12">Acciones & Gestión S.A.S</h2>
+            </div>
+          </div>
+          <div className="wrapper2">
+            <div className="group1">
+              <div className="wrapper21">
+                <div className="wrapper3">
+                  <h3 className="subtitle">Ajustes</h3>
+                  <div className="wrapper4">
+                    <div className="text">Verlos todos</div>
+                    <img className="image" src="../assets/a115a0c8bb34a775dada55e524af4e69.svg" alt="alt text" />
+                  </div>
+                </div>
+                <div className="wrapper5">
+                  <div className="wrapper6">
+                    <h2 className="wrapper61">Cambiar Correo</h2>
+                  </div>
+                  <div className="wrapper7">
+                    <div className="info">Nuevo Correo</div>
+                    <div className="box">
+                      <div className="wrapper8">
+                        <div className="wrapper81">
+                          <h5 className="wrapper82">user@gmail.com</h5>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="wrapper62">
+                  <h2 className="wrapper63">Home Address</h2>
+                </div>
+                <div className="wrapper9">
+                  <div className="info1">Pais de Residencia</div>
+                  <div className="box1">
+                    <div className="wrapper10">
+                      <img className="wrapper101" src="../assets/c798fb3edb88e1b7d3b264660d23b13f.svg" alt="alt text" />
+                    </div>
+                    <div className="wrapper83">
+                      <div className="wrapper84">
+                        <h5 className="wrapper85">USA</h5>
+                      </div>
+                    </div>
+                    <div className="wrapper102">
+                      <img className="wrapper101" src="../assets/44443f1892b458605d43d8b5389ba615.svg" alt="alt text" />
+                    </div>
+                  </div>
+                </div>
+                <div className="wrapper14">
+                  <div className="wrapper141">
+                    <div className="wrapper15">
+                      <div className="wrapper16">
+                        <div className="info2">Address Line</div>
+                        <div className="box2">
+                          <div className="wrapper86">
+                            <div className="wrapper87">
+                              <h5 className="wrapper88">Direccion</h5>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                      <div className="wrapper17">
+                        <div className="info3">Direccion Postal</div>
+                        <div className="box3">
+                          <div className="wrapper89">
+                            <div className="wrapper810">
+                              <h5 className="wrapper811">Ex: 0000</h5>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="wrapper18">
+                      <div className="info4">Ciudad</div>
+                      <div className="box4">
+                        <div className="wrapper812">
+                          <div className="wrapper813">
+                            <h5 className="wrapper814">Ciudad, Estado</h5>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="rect4"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="wrapper19">
+        <div className="box5">
+          <div className="wrapper20">
+            <img className="wrapper101" src="../assets/a770d6d4dc1b53b891948216a1fa1beb.svg" alt="alt text" />
+            <div className="text1">Panel de Control</div>
+          </div>
+        </div>
+        <div className="wrapper22">
+          <div className="wrapper221">
+            <img className="wrapper101" src="../assets/53f6262d2a8682c5a9fd6e4ef1d4c50f.svg" alt="alt text" />
+            <div className="text2">Mercado</div>
+          </div>
+        </div>
+        <div className="wrapper24">
+          <div className="wrapper241">
+            <img className="wrapper101" src="../assets/6aa7e007a39dd840c35a6a555ebed9af.svg" alt="alt text" />
+            <div className="text3">Transacciones</div>
+          </div>
+        </div>
+        <div className="box6">
+          <div className="wrapper26">
+            <img className="wrapper101" src="../assets/d782679ec45cc8d5ed1db8b8f8f7b102.svg" alt="alt text" />
+            <div className="text4">Ajustes</div>
+          </div>
+        </div>
+      </div>
+      <img className="decorator2" src="../assets/e5129eb20d09710fae0102792b3ecd0e.svg" alt="alt text" />
+    </div>
+  );
+}
+
+export default Ajustes;
